c256ef597eaf5730d459242ef2c0a350b2dfea9e
mv <mv@pop-os.lan>
2025-08-13 12:42:32 +0200

    First hour complete

--- code/basics.jl
+++ code/basics.jl
@@ -1,2 +1,16 @@
-using LinearAlgebra
+using LinearAlgebra, Distributions, Statistics, Plots
 
+# Julia as a calculator
+a = 2
+b = 3
+c = a + b
+sin(3)
+
+# Broadcasting
+x = [1, 2, 3]
+sin(x)  # gives an error sin(x) is not defined for arrays
+sin.(x) # broadcasting with dot operator. The sin function is applied to each element of x.
+
+# Plotting
+xgrid = -pi:0.01:pi # or π:0.01:π using Unicode
+plot(xgrid, sin.(xgrid), label="sin(x)", title="Sine Function", xlabel="x", ylabel="sin(x)")
